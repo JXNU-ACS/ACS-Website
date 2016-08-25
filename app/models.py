463d5d0bcef7faf13c6728a62e96ef3dbb8488d7
tyx <a1023122541@qq.com>
2016-08-25 19:47:13 +0800

    update models

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Student(db.Model):
     __tablename__='students'
     id = db.Column(db.Integer,primary_key=True)
     stu_id = db.Column(db.BigInteger,unique=True)
-    name = db.Column(db.String(8))
+    name = db.Column(db.String(16))
     stu_class = db.Column(db.String(64))
     qq = db.Column(db.String(20))
 
