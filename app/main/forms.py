d0966188bcee35e959433ba4723fbbe50138e98b
tyx <a1023122541@qq.com>
2016-08-27 11:27:56 +0800

    update some

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required,Length
 from ..models import Student
