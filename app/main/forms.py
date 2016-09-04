fda466d892559a20b5d5d5ce699d7eb974ec2094
tyx <a1023122541@qq.com>
2016-09-04 10:52:05 +0800

    add class information

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import Form
 from wtforms import StringField,SubmitField,SelectField
 from wtforms.validators import Required,Length
 from ..models import Student
-HOUR_CHOICES = [('0',u'计科师范班'),('1',u'计科1班'), ('2', u'计科2班'),('2',u'网工1班'),('3',u'网工2班'),('4',u'软件学院'),('5',u'其他院系')]
+HOUR_CHOICES = [('0',u'计科师范班'),('1',u'计科1班'), ('2', u'计科2班'),('2',u'网工1班'),('3',u'网工2班'),('4',u'物联网1班'),('5',u'物联网2班'),('6',u'软件学院'),('7',u'其他院系')]
 class StudentForm(Form):
     stu_id = StringField(u'学号')
     name = StringField(u'姓名')
