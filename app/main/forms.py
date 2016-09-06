b4711fb37bdd1fcf5c7fb199724efe665bc19e44
Ddragonever <ddragonever@gmail.com>
2016-09-06 19:14:03 +0800

    test version

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,12 @@
-# coding=utf-8
-from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required, Length
-from app.models import Student
-
-
+﻿from flask_wtf import Form
+from wtforms import StringField,SubmitField,SelectField
+from wtforms.validators import Required,Length
+from ..models import Student
+HOUR_CHOICES = [('0',u'计科师范班'),('1',u'计科1班'), ('2', u'计科2班'),('2',u'网工1班'),('3',u'网工2班'),('4',u'物联网1班'),('5',u'物联网2班'),('6',u'软件学院'),('7',u'其他院系')]
 class StudentForm(Form):
-    stuid = StringField(u'学号', validators=[Required(), Length(0,12)])
-    name = StringField(u'姓名', validators=[Required(), Length(1,10)])
-    banji = StringField(u'班级', validators=[Required()])
+    stu_id = StringField(u'学号')
+    name = StringField(u'姓名')
+    stu_class = SelectField(u'班级',choices=HOUR_CHOICES)
     qq = StringField('QQ')
     submit = SubmitField(u'确认')
+
--- app/main/views.py
+++ app/main/views.py
@@ -1,23 +1,36 @@
-# coding=utf-8
+#coding=utf-8
 from flask import render_template,flash,url_for,redirect
-from app.main.forms import StudentForm
-from app.models import Student, db
-from app.main import main
+from .forms import StudentForm
+from ..models import Student,db
+from . import main
+import requests
+import json
 
 
-@main.route('/', methods=['GET','POST'])
+
+
+@main.route('/',methods=['GET','POST'])
 def index():
     form = StudentForm()
     if form.validate_on_submit():
-        student = Student.query.filter_by(stuid=form.stuid.data).first()
-        if student is None:
-            student = Student(stuid=form.stuid.data, name=form.name.data,qq=form.qq.data,banji=form.banji.data)
-            db.session.add(student)
-            flash(u'报名成功！！！')
+        student = Student.query.filter_by(stu_id=form.stu_id.data).first()
+        
+        if student is  None:
+            url = "http://www.jxnugo.com/api/is_jxnu_student"
+            post_data = { 'student_id': form.stu_id.data }
+            headers = {'content-type': 'application/json'}
+            r = requests.post(url, data=json.dumps(post_data), headers=headers)
+            if form.name.data == r.json()['student_name']: 
+                student = Student(stu_id=form.stu_id.data,name=form.name.data,qq=form.qq.data,stu_class=form.stu_class.data)
+                flash(u'报名成功！！！')
+                db.session.add(student)
+            else:
+                flash(u'所填姓名和学号不符！！！')
+                redirect(url_for('main.index'))
         else:
-            flash(u'你已经报过名了！！！')
+            flash(u'你已经报过名了，请勿多次点击！！！')
         redirect(url_for('main.index'))
-    return render_template('index.html', form=form)
+    return render_template('website.html',form=form)
 
 
 
