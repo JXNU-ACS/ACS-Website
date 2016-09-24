2896fdd3a19353c679b6ca68ca5d7aac7a5d96f8
william-tu <a1023122541@qq.com>
2016-09-24 15:41:05 +0800

    update css mainly

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import Form
 from wtforms import StringField,SubmitField,SelectField,IntegerField
 from wtforms.validators import Required,Length
 from ..models import Student
-HOUR_CHOICES = [('0',u'计科师范班'),('1',u'计科1班'), ('2', u'计科2班'),('2',u'网工1班'),('3',u'网工2班'),('4',u'物联网1班'),('5',u'物联网2班'),('6',u'软件学院'),('7',u'其他院系')]
+HOUR_CHOICES = [('0',u'计科师范1班'),('1',u'计科师范2班'),('2', u'计科师范3班'),('3',u'计科1班'), ('4', u'计科2班'),('5',u'网工1班'),('6',u'网工2班'),('7',u'物联网1班'),('8',u'物联网2班'),('9',u'软件学院'),('10',u'其他院系')]
 class StudentForm(Form):
     stu_id = IntegerField(u'学号')
     name = StringField(u'姓名')
