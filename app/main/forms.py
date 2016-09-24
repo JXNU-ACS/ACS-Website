da48a0c19bc56c5370df6c66b17f910404c47d29
william-tu <a1023122541@qq.com>
2016-09-24 15:26:49 +0800

    update

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,11 @@
 ﻿#coding=utf-8
 from flask_wtf import Form
-from wtforms import StringField,SubmitField,SelectField
+from wtforms import StringField,SubmitField,SelectField,IntegerField
 from wtforms.validators import Required,Length
 from ..models import Student
 HOUR_CHOICES = [('0',u'计科师范班'),('1',u'计科1班'), ('2', u'计科2班'),('2',u'网工1班'),('3',u'网工2班'),('4',u'物联网1班'),('5',u'物联网2班'),('6',u'软件学院'),('7',u'其他院系')]
 class StudentForm(Form):
-    stu_id = StringField(u'学号')
+    stu_id = IntegerField(u'学号')
     name = StringField(u'姓名')
     stu_class = SelectField(u'班级',choices=HOUR_CHOICES)
     qq = StringField('QQ')
