655e1a9644d644681e36e25081c8c8bb93c18993
william-tu <a1023122541@qq.com>
2017-07-15 19:56:40 +0800

    format code and add ajax

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
-
 from . import db
 
+
 class Student(db.Model):
-    __tablename__='students'
-    id = db.Column(db.Integer,primary_key=True)
-    stu_id = db.Column(db.BigInteger,unique=True)
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.BigInteger, unique=True)
     name = db.Column(db.String(16))
     stu_class = db.Column(db.String(64))
     qq = db.Column(db.String(20))
